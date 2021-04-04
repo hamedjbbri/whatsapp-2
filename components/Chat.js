ff702c0c4bb14f29d285a51f2eb1351ac8bc2ed9
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-04-04 08:47:42 -0700

    Chat Screen

--- components/Chat.js
+++ components/Chat.js
@@ -4,17 +4,23 @@ import styled from "styled-components";
 import getRecipientEmail from '../utils/getRecipientEmail';
 import { auth , db} from '../firebase';
 import { useCollection } from "react-firebase-hooks/firestore";
+import { useRouter } from "next/router";
 
 
 function Chat({ id, users }) {
+    const router = useRouter();
      const [user] = useAuthState(auth);
      const [recipientSnapshot] = useCollection(db.collection('users').where('email', '==', getRecipientEmail(users,user)))
+
+     const enterChat = () => {
+         router.push(`/chat/${id}`)
+     }
 
     const recipient = recipientSnapshot?.docs?.[0]?.data(); 
     const recipientEmail = getRecipientEmail(users, user)
 
     return  (
-        <Container>
+        <Container onClick={enterChat}>
             {recipient ? (
                 <UserAvatar src={recipient?.photoURL} />
             ) : ( 
---
+++ pages/chat/[id].js
@@ -0,0 +1,31 @@
+import Head from "next/head"
+import styled from "styled-components";
+import ChatScreen from "../../components/ChatScreen";
+import Sidebar from "../../components/Sidebar";
+
+function Chat() {
+    return (
+        <Container>
+            <Head>
+              <title>
+                 chat    
+              </title>  
+
+            </Head>
+             
+            <Sidebar />
+            <ChatContainer>
+                <ChatScreen />
+            </ChatContainer>
+        </Container>
+    )
+}
+
+export default Chat
+ 
+
+const Container = styled.div`
+  display: flex;
+`;
+
+const ChatContainer = styled.div``;
