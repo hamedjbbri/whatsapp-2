95cc6875c4c525a58550e45e8392088aa0530064
Hamed Jabbari <jabbary.hamed@gmail.com>
2021-04-03 19:38:02 -0700

    Recipient part

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -4,48 +4,67 @@ import ChatIcon from '@material-ui/icons/Chat';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
 import SearchIcon from '@material-ui/icons/Search';
 import * as EmailValidator from "email-validator";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { useCollection } from "react-firebase-hooks/firestore";
+import { auth, db } from "../firebase";
+import Chat from "./Chat";
+
 
 function Sidebar() {
 
-    const createChat = () => {
-         const input = prompt("Please enter an email address for the user you wish to chat with");
+  const [user] = useAuthState(auth);
 
-         if(!input) return null;
-       
-         if (EmailValidator.validate(input)) {
-             // we need to add the chat into the DB 'chats' collection
-             
-          }
+  const userChatRef = db.collection('chats').where('users', 'array-contains', user.email)
+  const [chatSnapshot] = useCollection(userChatRef);
 
+
+  const createChat = () => {
+    const input = prompt("Please enter an email address for the user you wish to chat with");
+
+    if (!input) return null;
+
+    if (EmailValidator.validate(input) && !chatAlreadyExists(input) && input !== user.email) {
+      // we need to add the chat into the DB 'chats' collection
+      db.collection('chats').add({
+        users: [user.email, input],
+      })
     }
+  };
 
-    return (
-        <Container>
-          <Header>
-              <UserAvatar />
-             
-              <IconsContainer>
-                  <IconButton>
-                     <ChatIcon />
-                  </IconButton>
-                  <IconButton>
-                    <MoreVertIcon /> 
-                  </IconButton>
-              </IconsContainer>
-          </Header>
+  const chatAlreadyExists = (recipientEmail) =>
+    !!chatSnapshot?.docs.find((chat) => chat.data().users.find(user => user === recipientEmail)?.length > 0
+    
+    );
 
-         <Search>
-      
-              <SearchIcon /> 
-              <SearchInput placeholder="Search in chats" />
-         </Search>
 
-         <SidebarButton onClick={createChat}>Start a new chat</SidebarButton>
+  return (
+    <Container>
+      <Header>
+        <UserAvatar onClick={() => auth.signOut()} />
+        <IconsContainer>
+          <IconButton>
+            <ChatIcon />
+          </IconButton>
+          <IconButton>
+            <MoreVertIcon />
+          </IconButton>
+        </IconsContainer>
+      </Header>
 
-         {/* List of Chats */}
+      <Search>
+        <SearchIcon />
+        <SearchInput placeholder="Search in chats" />
+      </Search>
 
-        </Container>
-    )
+      <SidebarButton onClick={createChat}>Start a new chat</SidebarButton>
+      {/* List of Chats */}
+
+      {chatSnapshot?.docs.map(chat => (
+        <Chat key={chat.id} id={chat.id} user={chat.data().users} />
+      ))}
+
+    </Container>
+  )
 }
 
 export default Sidebar;
